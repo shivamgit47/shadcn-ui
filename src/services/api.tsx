40dd9346eefddcfb3f0bde278089dff26d691e3f
shivam <shivamnov21@gmail.com>
2025-05-06 11:39:39 +0530

    fifth commit

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const URL="https://fakestoreapi.com";
 
-//creating axios instanceS
+//creating axios instanceS  
 const axiosInstance = axios.create({
     baseURL:"https://fakestoreapi.com",
     headers:{
